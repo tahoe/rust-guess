55592a3321053315c02711a2b08b3bec87e60510
Dennis Durling <djdtahoe@gmail.com>
2018-02-07 12:48:40 -0800

    more examples in the docs including help output when no argument given and default for when bad argument is given

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,32 @@
 //! I will default to 128 and you will get however many guesses
 //! 128 requires
 //!
-//! #Examples
+//! # Help output when nothing is given
+//!
+//! ```
+//! error: The following required arguments were not provided:
+//!     <num>
+//!
+//! USAGE:
+//!     gg <num>
+//!
+//! For more information try --help
+//! ```
+//!
+//! # Default when input is broken
+//! ```
+//! gg ff?
+//! Let's play "Guess that numbrer!" 7
+//! I'm thinking of a number from 1 to 128
+//! You get 7 chances before I start calling you names
+//! Chance 1, Please input your guess:
+//! ```
+//!
+//! # Examples
 //!
 //! ```
 //! gg 500
-//! Let's play "Guess that number"
+//! Let's play "Guess that number!"
 //! I'm thinking of a number from 1 to 500
 //! You get 9 chances before I start calling you names
 //! Chance 1, Please input your guess:
